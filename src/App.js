dceaf6cfd3b00e8f6f3aabbc467a87899060c17f
ga8226 <ga8226@naver.com>
2023-07-05 18:03:23 +0900

    씁 뭐지이거

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import { useState } from "react";
+import'./App.css'
 
 function App() {
-const [count,setCount] =useState(0)
-let count2= 0;
-const timer = setInterval(()=>{})
-
-
-
-
+  const bgcolor = ['red','green','blue']
+ const [colovar , setcolor] = useState(0)
   return (
    <>
-   <button onClick={()=>{setCount(count + 1)}}>{count}</button>
-   <button onClick={()=>{count2++ }}>{count2}</button>
+   <button  //리액트 데이터 넣어주는 {} 하이픈 있으면 안되고 붙어있어야 해서 안 쓴다
+   onClick={()=>{
+    setcolor((colovar + 1) % bgcolor.length)
+    
+   }}>
+    {document.body.classList = bgcolor[colovar]}</button>
    </>
   );
 }
